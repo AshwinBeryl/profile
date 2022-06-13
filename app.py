e5da35c9b466026d5300ff6d6defeac860348f44
AshwinBeryl <ashwinberyl@gmail.com>
2022-06-13 16:58:49 +0530

    added route for ststic path

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from encodings.utf_8 import encode
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
+
+@app.route("/static/<path:path>")
+def static_dir(path):
+    return send_from_directory("static", path)
 
 @app.route('/', methods=['POST','GET'])
 def home():
